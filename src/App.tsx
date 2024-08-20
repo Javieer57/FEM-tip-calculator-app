a47fd337a3d018bb481c85aee78d6cf37968c0d7
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2024-08-20 13:29:08 -0600

    add percent inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+import { PercentButton } from "./components/PercentButton";
+import { CustomPercent } from "./components/CustomPercent";
+
 function App() {
-  return <></>;
+  const [customPercent, setCustomPercent] = useState("");
+  const [selectedPercent, setSelectedPercent] = useState<string | null>(null);
+  const percents: string[] = ["5", "10", "15", "25", "50"];
+
+  const handleSelectedPercentChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setSelectedPercent(e.target.value);
+    setCustomPercent("");
+  };
+
+  const handleCustomPercentChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setCustomPercent(e.target.value);
+
+    if (e.target.value) {
+      setSelectedPercent(null);
+    }
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
+        {percents.map((percent) => (
+          <PercentButton
+            key={percent}
+            percent={percent}
+            selectedRadio={selectedPercent}
+            handleSelectedPercentChange={handleSelectedPercentChange}
+          />
+        ))}
+
+        <CustomPercent
+          customPercent={customPercent}
+          handleCustomPercentChange={handleCustomPercentChange}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
