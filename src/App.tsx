b1e8f2cd9886afe112b0d8556b4d99c6a80e7c02
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2024-08-21 10:42:21 -0600

    add inputs components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { PercentButton } from "./components/PercentButton";
 import { CustomPercent } from "./components/CustomPercent";
+import { PeopleInput } from "./components/PeopleInput";
+import { BillInput } from "./components/BillInput";
 
 function App() {
   const [bill, setBill] = useState<string>("");
+  const [people, setPeople] = useState<string>("");
   const [customPercent, setCustomPercent] = useState("");
   const [selectedPercent, setSelectedPercent] = useState<string | null>(null);
   const percents: string[] = ["5", "10", "15", "25", "50"];
@@ -18,6 +21,7 @@ function App() {
   const handleCustomPercentChange = (
     e: React.ChangeEvent<HTMLInputElement>,
   ) => {
+    if (!e.target.validity.valid) return;
     setCustomPercent(e.target.value);
 
     if (e.target.value) {
@@ -26,38 +30,18 @@ function App() {
   };
 
   const handleBillChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.validity.valid) return;
     setBill(e.target.value);
+  };
+
+  const handlePeopleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.validity.valid) return;
+    setPeople(e.target.value);
   };
 
   return (
     <>
-      <div>
-        <label
-          className="mb-2 inline-block font-bold text-[#5E7A7D]"
-          htmlFor="bill"
-        >
-          Bill
-        </label>
-
-        <div className="relative">
-          <img
-            className="absolute left-4 top-1/2 h-[1.0625rem] w-[0.6875rem] -translate-y-1/2"
-            src="/icon-dollar.svg"
-            alt=""
-            width={11}
-            height={17}
-          />
-          <input
-            type="text"
-            name="bill"
-            id="bill"
-            className="inline-block w-full rounded bg-[#f3f8fb] p-2 px-4 pb-3 pl-10 text-right text-2xl font-bold text-[#00494D] placeholder:text-[#9EBBBD]"
-            placeholder="0"
-            value={bill}
-            onChange={handleBillChange}
-          />
-        </div>
-      </div>
+      <BillInput bill={bill} handleBillChange={handleBillChange} />
 
       <fieldset>
         <legend className="mb-4 inline-block font-bold text-[#5E7A7D]">
@@ -80,6 +64,8 @@ function App() {
           />
         </div>
       </fieldset>
+
+      <PeopleInput people={people} handlePeopleChange={handlePeopleChange} />
     </>
   );
 }
