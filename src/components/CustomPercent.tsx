07f5463957bd554768c8f3742f7a2f30a83f7984
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2024-08-21 16:53:29 -0600

    fix custom percent height

--- src/components/CustomPercent.tsx
+++ src/components/CustomPercent.tsx
@@ -8,19 +8,19 @@ export const CustomPercent = ({
   handleCustomPercentChange,
 }: CustomPercentButtonProps) => {
   return (
-    <>
+    <div>
       <label className="sr-only" htmlFor="custom-percent"></label>
       <input
         type="text"
         inputMode="numeric"
         pattern="[0-9]*"
         name="custom-percent"
-        className="inline-block w-full rounded bg-[#f3f8fb] p-2 px-4 pb-3 text-right text-2xl font-bold text-[#00494D] placeholder:text-[#9EBBBD]"
+        className="inline-block w-full rounded bg-[#f3f8fb] p-2 px-4 pb-3 text-right text-2xl font-bold leading-8 text-[#00494D] placeholder:text-[#9EBBBD]"
         id="custom-percent"
         placeholder="Custom"
         value={customPercent}
         onChange={handleCustomPercentChange}
       />
-    </>
+    </div>
   );
 };
