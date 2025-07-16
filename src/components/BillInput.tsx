33597b430ff35043a75672eb74148c0f0de28681
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2025-07-16 11:51:50 -0600

    core: update tailwindcss v4

--- src/components/BillInput.tsx
+++ src/components/BillInput.tsx
@@ -21,7 +21,7 @@ export function BillInput({ bill, setBill }: BillInputProps) {
 
       <div className="relative">
         <img
-          className="absolute left-4 top-1/2 h-[1.0625rem] w-[0.6875rem] -translate-y-1/2"
+          className="absolute left-4 top-1/2 h-4.25 w-2.75 -translate-y-1/2"
           src={IconDollar}
           alt=""
           width={11}
@@ -33,7 +33,7 @@ export function BillInput({ bill, setBill }: BillInputProps) {
           pattern="^\d*\.?\d*$"
           name="bill"
           id="bill"
-          className="inline-block w-full rounded border-2 border-transparent bg-dark-cyan-200 p-2 px-4 pb-3 pl-10 text-right text-2xl font-bold text-dark-cyan-900 outline-none placeholder:text-dark-cyan-300 focus:border-cyan-400"
+          className="inline-block w-full rounded-sm border-2 border-transparent bg-dark-cyan-200 p-2 px-4 pb-3 pl-10 text-right text-2xl font-bold text-dark-cyan-900 outline-hidden placeholder:text-dark-cyan-300 focus:border-cyan-400"
           placeholder="0"
           value={bill}
           onChange={handleBillChange}
--- src/components/PeopleInput.tsx
+++ src/components/PeopleInput.tsx
@@ -25,7 +25,7 @@ export const PeopleInput = ({ people, setPeople }: PeopleInputProps) => {
 
       <div className="relative">
         <img
-          className="absolute left-4 top-1/2 h-4 w-[0.8125rem] -translate-y-1/2"
+          className="absolute left-4 top-1/2 h-4 w-3.25 -translate-y-1/2"
           src={IconPerson}
           alt=""
           width={13}
@@ -37,7 +37,7 @@ export const PeopleInput = ({ people, setPeople }: PeopleInputProps) => {
           pattern="[0-9]*"
           name="people"
           id="people"
-          className={`inline-block w-full rounded border-2 bg-dark-cyan-200 p-2 px-4 pb-3 pl-10 text-right text-2xl font-bold text-dark-cyan-900 outline-none placeholder:text-dark-cyan-300 ${people === "0" ? "border-orange" : "border-transparent focus:border-cyan-400"}`}
+          className={`inline-block w-full rounded-sm border-2 bg-dark-cyan-200 p-2 px-4 pb-3 pl-10 text-right text-2xl font-bold text-dark-cyan-900 outline-hidden placeholder:text-dark-cyan-300 ${people === "0" ? "border-orange" : "border-transparent focus:border-cyan-400"}`}
           placeholder="0"
           value={people}
           onChange={handlePeopleChange}
