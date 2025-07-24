6dfb6a343d64a27f7173392141d780ec5c5dfec1
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2025-07-23 18:21:04 -0600

    feat: add basic stories to CustomPercent component

---
+++ src/components/CustomPercent.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { CustomPercent } from "./CustomPercent";
+import { fn } from "storybook/test";
+
+export const ActionsData = {
+  handleCustomPercentChange: fn(),
+};
+
+const meta = {
+  component: CustomPercent,
+  title: "CustomPercent",
+  tags: ["autodocs"],
+  //👇 Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+  args: {
+    ...ActionsData,
+  },
+} satisfies Meta<typeof CustomPercent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    customPercent: "",
+  },
+};
+
+export const Filled: Story = {
+  args: {
+    customPercent: "12",
+  },
+};
