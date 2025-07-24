6648e56e72490680d2ba635aa88468e0e9e8ece6
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2025-07-24 05:53:53 -0600

    styles: use has-checked for styles

--- src/components/PercentButton.tsx
+++ src/components/PercentButton.tsx
@@ -15,18 +15,18 @@ export const PercentButton = ({
   return (
     <label
       htmlFor={`id-${percent}`}
-      className="bg-dark-cyan-900 hover:text-dark-cyan-900 inline-block w-full rounded-sm p-2 pb-3 text-center text-2xl font-bold text-white hover:bg-cyan-200 has-checked:bg-cyan-400"
+      className="bg-dark-cyan-900 hover:text-dark-cyan-900 has-checked:text-dark-cyan-900 inline-block w-full rounded-sm p-2 pb-3 text-center text-2xl font-bold text-white hover:bg-cyan-200 has-checked:bg-cyan-400"
     >
       <input
         type="radio"
         name="tip-percent"
         id={`id-${percent}`}
-        className="peer sr-only"
+        className="sr-only"
         value={percent}
         checked={isSelected}
         onChange={(e) => onChange(e.target.value)}
       />
-      <span className="peer-checked:text-dark-cyan-900">{percent}%</span>
+      <span>{percent}%</span>
     </label>
   );
 };
