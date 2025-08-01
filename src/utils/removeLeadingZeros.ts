334ba3641690f5d3347c526737c194dfb6e20589
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2025-08-01 07:40:51 -0600

    fix removing leading zeros on bill input

--- src/utils/removeLeadingZeros.ts
+++ src/utils/removeLeadingZeros.ts
@@ -1,3 +1,4 @@
 export const removeLeadingZeros = (value: string): string => {
-  return Number(value).toString();
+  // https://stackoverflow.com/questions/594325/input-field-value-remove-leading-zeros
+  return value.replace(/^0+(?!\.|$)/, "");
 };
