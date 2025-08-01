060b157d8cc4418f6e3d1f6141c3a9316a0d33d5
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2025-08-01 07:43:50 -0600

    remove redundant html for

--- src/components/PercentButton.tsx
+++ src/components/PercentButton.tsx
@@ -16,14 +16,10 @@ export const PercentButton = ({ percent }: PercentButtonProps) => {
   };
 
   return (
-    <label
-      htmlFor={`id-${percent}`}
-      className="bg-dark-cyan-900 hover:text-dark-cyan-900 has-checked:text-dark-cyan-900 inline-block w-full rounded-sm p-2 pb-3 text-center text-2xl font-bold text-white select-none hover:bg-cyan-200 has-checked:bg-cyan-400 has-focus-visible:bg-cyan-200"
-    >
+    <label className="bg-dark-cyan-900 hover:text-dark-cyan-900 has-checked:text-dark-cyan-900 inline-block w-full rounded-sm p-2 pb-3 text-center text-2xl font-bold text-white select-none hover:bg-cyan-200 has-checked:bg-cyan-400 has-focus-visible:bg-cyan-200">
       <input
         type="radio"
         name="tip-percent"
-        id={`id-${percent}`}
         className="sr-only"
         value={percent}
         checked={selectedPercent === percent.toString()}
